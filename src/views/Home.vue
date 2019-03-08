a7794a022dc27eb28fb6edcd6a4b4140e3dd692f
anke <245783065@qq.com>
2019-03-08 19:16:41 +0800

    resetCss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
-    AAA
+    <!-- <div id="read"></div> -->
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
+import Epub from 'epubjs'
+global.ePub = Epub
 export default {
-  name: 'home'
+  name: 'home',
+  mounted () {
+    this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
+  }
 }
 </script>
