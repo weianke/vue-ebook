44f4dbac0833364c1fc088f151744fdb8b406c2d
anke <245783065@qq.com>
2019-03-20 16:25:20 +0800

    i18n

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,51 @@
 <template>
   <div id="app">
-    <span class="text">ADOAMO</span>
+    <span class="text" v-text="$t('m.music')"></span>
+    <div class="top_btn"
+         @click="changeLangEvent">中文</div>
     <router-view/>
   </div>
 </template>
 
 <script>
-  import { mapActions, mapGetters } from 'vuex'
-  export default {
-    mounted() {
-      // this.$store.dispatch('setTest', 32).then(() => {
-      //    console.log(this.book)
-      // })
-      this.setTest('23424')
-    },
-    methods: {
-      ...mapActions([
-        'setTest'
-      ])
-    },
-    computed: {
-      ...mapGetters([
-        'book'
-      ])
-    }
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  mounted () {
+    // this.$store.dispatch('setTest', 32).then(() => {
+    //    console.log(this.book)
+    // })
+    this.setTest('23424')
+  },
+  methods: {
+    ...mapActions([
+      'setTest'
+    ]),
+    changeLangEvent () {
+      if (this.$i18n.locale === 'zh-CN') {
+        this.$i18n.locale = 'en-US'//关键语句
+        console.log('en-US')
+      } else {
+        this.$i18n.locale = 'zh-CN'//关键语句
+        console.log('zh-CN')
+      }
   }
-  document.addEventListener('DOMContentLoaded', () => {
-    const html = document.querySelector('html')
-    let fontSize = window.innerWidth / 10
-    fontSize = fontSize > 50 ? 50 : fontSize
-    html.style.fontSize = fontSize + 'px'
-  })
+},
+computed: {
+    ...mapGetters([
+    'book'
+  ])
+}
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/styles/mixin.scss";
+@import '@/assets/styles/mixin.scss';
 .text {
   font-size: px2rem(20);
   color: orange;
--- src/main.js
+++ src/main.js
@@ -2,14 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueI18n from 'vue-i18n'
 import '@/assets/styles/icon.css'
 import '@/assets/styles/global.scss'
 import '@/assets/fonts/daysOne.css'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
+
+const i18n = new VueI18n({
+  locale: 'zh-CN', // 语言标识, 通过切换locale的值来实现语言切换,this.$i18n.locale 
+  messages: {
+    'zh-CN': require('./lang/zh'), // 中文语言包
+    'en-US': require('./lang/en') // 英文语言包
+  }
+})
 
 new Vue({
   router,
+  i18n,
   store,
   render: h => h(App)
 }).$mount('#app')
