954bbaf1911a7380e2411bdca001599369b447dc
anke <245783065@qq.com>
2019-06-18 19:31:35 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -13,23 +13,5 @@ module.exports = {
     hotOnly: false,
     proxy: null, // 设置代理
     before: app => {}
-  },
-  configureWebpack: {
-    plugins: [
-      new SkeletonPlugin({
-        pathname: path.resolve(__dirname, './shell'), // 用来存储 shell 文件的地址
-        staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
-        routes: ['/'], // 将需要生成骨架屏的路由添加到数组中
-        excludes: ['.van-nav-bar', '.van-tabbar']
-      })
-    ]
-  },
-  chainWebpack: config => {  // 解决vue-cli3脚手架创建的项目压缩html 干掉<!-- shell -->导致骨架屏不生效
-    if (process.env.NODE_ENV !== 'development') {
-      config.plugin('html').tap(opts => {
-        opts[0].minify.removeComments = false
-        return opts
-      })
-    }
   }
 }
